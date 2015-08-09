29e0f194f0cf83cd1eb9477b21f3d21e49343a67
Alexandre Mutel <alexandre_mutel@live.com>
2015-08-09 23:46:27 +0900

    Add RocketException

---
+++ src/RocketException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GitRocketFilterBranch
+{
+    /// <summary>
+    /// An exception used to display errors (compilation, invalid parameters...etc).
+    /// </summary>
+    public class RocketException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Exception" /> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public RocketException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RocketException"/> class.
+        /// </summary>
+        /// <param name="formatMessage">The format message.</param>
+        /// <param name="args">The arguments.</param>
+        public RocketException(string formatMessage, params object[] args)
+            : base(string.Format(formatMessage, args))
+        {
+        }
+    }
+}
