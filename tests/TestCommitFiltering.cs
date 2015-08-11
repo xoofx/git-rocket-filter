6023b0076ad7f15e9b504c80bdfe73e55caaae31
Alexandre Mutel <alexandre_mutel@live.com>
2015-08-11 23:21:23 +0900

    Use helper methods for TestCommitFiltering

--- tests/TestCommitFiltering.cs
+++ tests/TestCommitFiltering.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using LibGit2Sharp;
 using Xunit;
 
@@ -14,6 +15,7 @@ namespace GitRocketFilter.Tests
         {
             var test = InitializeTest();
 
+            // Test directly the main program as we want to test also command line parameters
             Program.Main("--commit-filter",
                 @"commit.Message += ""This is a test"";",
                 "--repo-dir", test.Path,
@@ -22,11 +24,9 @@ namespace GitRocketFilter.Tests
 
             var repo = test.Repo;
             var headNewMaster = AssertBranchRef(repo);
-                
-            var originalCommits = repo.Commits.QueryBy(new CommitFilter() { SortBy = CommitSortStrategies.Topological })
-                .ToList();
-            var newCommits = repo.Commits.QueryBy(new CommitFilter() { Since = headNewMaster, SortBy = CommitSortStrategies.Topological })
-                .ToList();
+
+            var originalCommits = GetCommits(repo);
+            var newCommits = GetCommits(repo, headNewMaster);
 
             // Make sure that we have the same number of commits
             Assert.Equal(originalCommits.Count, newCommits.Count);
@@ -42,6 +42,56 @@ namespace GitRocketFilter.Tests
                 // Check the new message
                 Assert.EndsWith("This is a test", commit.Message);
                 Assert.StartsWith(originalCommit.Message, commit.Message);
+            }
+
+            // Cleanup the test only if we succeed
+            test.Dispose();
+        }
+
+        /// <summary>
+        /// Appends the message "This is a test" to all commits.
+        /// </summary>
+        [Fact]
+        public void ModifyCommitMessageLast4()
+        {
+            var test = InitializeTest();
+
+            // Test directly the main program as we want to test also command line parameters
+            Program.Main("--commit-filter",
+                @"commit.Message += ""This is a test"";",
+                "--repo-dir", test.Path,
+                "--branch", NewBranch,
+                @"HEAD~4..HEAD");
+
+            var repo = test.Repo;
+            var headNewMaster = AssertBranchRef(repo);
+
+            var originalCommits = GetCommits(repo);
+            var originalCommitsRange = GetCommitsFromRange(repo, @"HEAD~4..HEAD").Select(commit => commit.Id).ToList();
+            var newCommits = GetCommits(repo, headNewMaster);
+
+            // Make sure that we have the same number of commits
+            Assert.Equal(originalCommits.Count, newCommits.Count);
+
+            for (int i = 0; i < newCommits.Count; i++)
+            {
+                var originalCommit = originalCommits[i];
+                var commit = newCommits[i];
+
+                if (originalCommitsRange.Contains(originalCommit.Id))
+                {
+                    // All commits should have same tree id
+                    Assert.Equal(originalCommit.Tree.Id, commit.Tree.Id);
+
+                    // Check the new message
+                    Assert.EndsWith("This is a test", commit.Message);
+                    Assert.StartsWith(originalCommit.Message, commit.Message);
+                }
+                else
+                {
+                    // All commits should have same tree id
+                    Assert.Equal(originalCommit.Id, commit.Id);
+                }
             }
 
             // Cleanup the test only if we succeed
