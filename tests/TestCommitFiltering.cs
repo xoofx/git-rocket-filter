6ddeb39df39e81e3173885fda51ff713dda2db99
Alexandre Mutel <alexandre_mutel@live.com>
2015-08-11 23:01:01 +0900

    Add test project

---
+++ tests/TestCommitFiltering.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using LibGit2Sharp;
+using Xunit;
+
+namespace GitRocketFilter.Tests
+{
+    public class TestCommitFiltering : TestRepoBase
+    {
+        /// <summary>
+        /// Appends the message "This is a test" to all commits.
+        /// </summary>
+        [Fact]
+        public void ModifyCommitMessageAll()
+        {
+            var test = InitializeTest();
+
+            Program.Main("--commit-filter",
+                @"commit.Message += ""This is a test"";",
+                "--repo-dir", test.Path,
+                "--branch", NewBranch,
+                @"HEAD");
+
+            var repo = test.Repo;
+            var headNewMaster = AssertBranchRef(repo);
+                
+            var originalCommits = repo.Commits.QueryBy(new CommitFilter() { SortBy = CommitSortStrategies.Topological })
+                .ToList();
+            var newCommits = repo.Commits.QueryBy(new CommitFilter() { Since = headNewMaster, SortBy = CommitSortStrategies.Topological })
+                .ToList();
+
+            // Make sure that we have the same number of commits
+            Assert.Equal(originalCommits.Count, newCommits.Count);
+
+            for (int i = 0; i < newCommits.Count; i++)
+            {
+                var originalCommit = originalCommits[i];
+                var commit = newCommits[i];
+
+                // All commits should have same tree id
+                Assert.Equal(originalCommit.Tree.Id, commit.Tree.Id);
+
+                // Check the new message
+                Assert.EndsWith("This is a test", commit.Message);
+                Assert.StartsWith(originalCommit.Message, commit.Message);
+            }
+
+            // Cleanup the test only if we succeed
+            test.Dispose();
+        }
+    }
+}
---
+++ tests/TestRepoBase.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using LibGit2Sharp;
+using Xunit;
+
+namespace GitRocketFilter.Tests
+{
+    public abstract class TestRepoBase
+    {
+        protected const string NewBranch = "new_master";
+        protected const string NewBranchRef = "refs/heads/new_master";
+
+        protected DisposeTempRepo InitializeTest([System.Runtime.CompilerServices.CallerMemberName] string memberName = "")
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            var repoName = GetType().Name + "_" + memberName;
+            var repoPath = Path.Combine(path, repoName);
+            if (Directory.Exists(repoPath))
+            {
+                RemoveDirectory(repoPath);
+            }
+            Directory.CreateDirectory(repoPath);
+            var repoSourcePath = Path.Combine(path, @"..\..\test_repo\");
+            DirectoryCopy(repoSourcePath, repoPath, true);
+            Directory.Move(Path.Combine(repoName, "dotgit"), Path.Combine(repoName, ".git"));
+            return new DisposeTempRepo(repoPath);
+        }
+
+        protected static string AssertBranchRef(Repository repo)
+        {
+            Assert.NotNull(repo.Refs[NewBranchRef]);
+            return repo.Refs[NewBranchRef].TargetIdentifier;
+        }
+
+        // http://stackoverflow.com/a/648055/1356325
+        private static void RemoveDirectory(string directoryPath)
+        {
+            if (directoryPath == null) throw new ArgumentNullException("directoryPath");
+            RemoveDirectory(new DirectoryInfo(directoryPath));
+        }
+
+        private static void RemoveDirectory(FileSystemInfo fileSystemInfo)
+        {
+            var directoryInfo = fileSystemInfo as DirectoryInfo;
+            if (directoryInfo != null)
+            {
+                foreach (var childInfo in directoryInfo.GetFileSystemInfos())
+                {
+                    RemoveDirectory(childInfo);
+                }
+            }
+
+            fileSystemInfo.Attributes = FileAttributes.Normal;
+            fileSystemInfo.Delete();
+        }
+
+        // https://msdn.microsoft.com/en-us/library/bb762914%28v=vs.110%29.aspx
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            // If the destination directory doesn't exist, create it. 
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location. 
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+
+        protected struct DisposeTempRepo : IDisposable
+        {
+            public DisposeTempRepo(string path)
+            {
+                Path = path;
+                Repo = new Repository(path);
+            }
+
+            public readonly string Path;
+
+            public readonly Repository Repo;
+
+            public void Dispose()
+            {
+                Repo.Dispose();
+                RemoveDirectory(Path);
+            }
+        }
+    }
+}
